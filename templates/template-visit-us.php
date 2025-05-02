23c1bd44e97c30b868915122ccc9553cba4947b9
meemal <naomispirit@gmail.com>
2025-05-02 13:29:25 +0100

    jouney news visit updated to use excerpt

--- templates/template-visit-us.php
+++ templates/template-visit-us.php
@@ -23,6 +23,17 @@ if (isset($what_3_words)){
             <?php the_excerpt(); ?>
         </div>
             <?php the_content(); ?>
+            <div class="w-full lg:w-2/3 mx-auto mt-[40px] sm:mt-[60px]">
+        <?php
+        $form_shortcode = get_field('form_shortcode');
+     echo  $form_shortcode;
+        if (!empty($form_shortcode)) {
+         
+            echo do_shortcode($form_shortcode);
+      
+        }
+        ?>
+        </div>
     </div>
 
 
